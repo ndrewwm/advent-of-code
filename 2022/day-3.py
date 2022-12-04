8d08f45984cb7bcd073925fec3d46a843b5f397b
ndrewwm <moore.andrew09@gmail.com>
2022-12-03 20:46:15 -0700

    Day 3, part 2

--- 2022/day-3.py
+++ 2022/day-3.py
@@ -3,8 +3,8 @@
 import string
 
 priority = {}
-for index, letter in enumerate(string.ascii_letters):
-    priority[letter] = 1 + index
+for index, letter in enumerate(string.ascii_letters, start = 1):
+    priority[letter] = index
 
 def split_rucksack(rucksack):
     midpt = len(rucksack) // 2
@@ -16,10 +16,16 @@ def split_rucksack(rucksack):
 def intersect_items(compartments):
     return list(set(compartments[0]).intersection(set(compartments[1])))[0]
 
+def intersect_elves(elves):
+    a, b, c = [set(elf) for elf in elves]
+
+    return list(a.intersection(b).intersection(c))[0]
+
 with open('day-3.txt') as file:
     lines = file.readlines()
     lines = [line.replace('\n', '') for line in lines]
 
+# part 1
 total = []
 for line in lines:
     comp = split_rucksack(line)
@@ -28,3 +34,10 @@ for line in lines:
 
 sum(total)
 
+# part 2
+groups = []
+for i in range(0, 299, 3):
+    badge = intersect_elves(lines[i:(i + 3)])
+    groups += [priority[badge]]
+
+sum(groups)
