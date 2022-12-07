78a3029767882f4a60d38dcf1bb921b63b50dbfe
ndrewwm <moore.andrew09@gmail.com>
2022-12-06 23:05:35 -0700

    Day 6, part 2

--- 2022/day-6.py
+++ 2022/day-6.py
@@ -1,15 +1,15 @@
 """Advent of Code 2022, Day 6"""
 
-def tune(signal):
+def tune(signal, nchar = 4):
     track = []
     counter = 0
     for char in signal:
         counter += 1
         track += [char]
-        if counter >= 4 and sorted(track) == sorted(list(set(track))):
+        if counter >= nchar and sorted(track) == sorted(list(set(track))):
             break
 
-        if len(track) == 4:
+        if len(track) == nchar:
             track.pop(0)
     
     return counter
@@ -17,4 +17,8 @@ def tune(signal):
 with open('day-6-input.txt') as file:
     d6input = file.readline()
 
-tune(d6input)
+# part 1
+tune(d6input, 4)
+
+# part 2
+tune(d6input, 14)
