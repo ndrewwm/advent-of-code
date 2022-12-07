2610b0fd066d4705755d889d2e1f1c6ae2fff069
ndrewwm <moore.andrew09@gmail.com>
2022-12-06 23:02:59 -0700

    Day 6, part 1 done

--- 2022/day-6.py
+++ 2022/day-6.py
@@ -1,2 +1,20 @@
 """Advent of Code 2022, Day 6"""
 
+def tune(signal):
+    track = []
+    counter = 0
+    for char in signal:
+        counter += 1
+        track += [char]
+        if counter >= 4 and sorted(track) == sorted(list(set(track))):
+            break
+
+        if len(track) == 4:
+            track.pop(0)
+    
+    return counter
+
+with open('day-6-input.txt') as file:
+    d6input = file.readline()
+
+tune(d6input)
